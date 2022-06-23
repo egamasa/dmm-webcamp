6cc49f8dd02a435f55657cd32caae0aef5377291
egamasa <40469431+egamasa@users.noreply.github.com>
2022-06-23 02:18:24 +0000

    SkillUp Rails guest login

--- bookers2-debug/app/models/user.rb
+++ bookers2-debug/app/models/user.rb
@@ -55,4 +55,12 @@ class User < ApplicationRecord
 
     @users = User.where("name LIKE?", value)
   end
+
+  # ゲストログイン
+  def self.guest
+    find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
 end
